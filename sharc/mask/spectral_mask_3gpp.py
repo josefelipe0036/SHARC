70067e8e974b0cf275ed5eebe72cb0b6e9d26bf7
José Felipe <75074939+josefelipe0036@users.noreply.github.com>
2025-07-09 15:35:59 -0300

    Add files via upload

--- sharc/mask/spectral_mask_3gpp.py
+++ sharc/mask/spectral_mask_3gpp.py
@@ -11,22 +11,25 @@ from sharc.mask.spectral_mask import SpectralMask
 import numpy as np
 import sys
 
+
 class SpectralMask3Gpp(SpectralMask):
 
-    def __init__(self,
-                 sta_type: StationType, 
-                 freq_mhz: float, 
-                 band_mhz: float, 
-                 spurious_emissions: float,
-                 scenario = "OUTDOOR"):
+    def __init__(
+        self,
+        sta_type: StationType,
+        freq_mhz: float,
+        band_mhz: float,
+        spurious_emissions: float,
+        scenario="OUTDOOR",
+    ):
         """
-        Implements spectral emission mask from 3GPP 36.104 Table 6.6.3.1-6 for 
+        Implements spectral emission mask from 3GPP 36.104 Table 6.6.3.1-6 for
         Wide Area BS operating with 5, 10, 15 or 20 MHz channel bandwidth.
-        
-        Also implements spectral emission mask from 3GPP 36.101 Table 6.6.2.1.1-1 
+
+        Also implements spectral emission mask from 3GPP 36.101 Table 6.6.2.1.1-1
         for UE operating with 5, 10, 15 or 20 MHz channel bandwidth.
-        
-        In order to characterize Cat-A or Cat-B base stations, it is necessary 
+
+        In order to characterize Cat-A or Cat-B base stations, it is necessary
         to adjust the input parameter that defines the spurious emission level:
             Cat-A: -13 dBm/MHz
             Cat-B: -30 dBm/MHz
@@ -35,11 +38,11 @@ class SpectralMask3Gpp(SpectralMask):
             message = "ERROR\nInvalid station type: " + str(sta_type)
             sys.stderr.write(message)
             sys.exit(1)
-            
-        if band_mhz not in [ 5, 10, 15, 20 ]:
+
+        if band_mhz not in [5, 10, 15, 20]:
             message = "ERROR\nInvalid bandwidth for 3GPP mask: " + band_mhz
             sys.stderr.write(message)
-            sys.exit(1)             
+            sys.exit(1)
 
         # Attributes
         self.spurious_emissions = spurious_emissions
@@ -47,28 +50,31 @@ class SpectralMask3Gpp(SpectralMask):
         self.scenario = scenario
         self.band_mhz = band_mhz
         self.freq_mhz = freq_mhz
-        
-        delta_f_lim = self.get_frequency_limits(self.sta_type, self.band_mhz)
-        #delta_f_lim_flipped = np.flip(self.delta_f_lim,0)
-        delta_f_lim_flipped = delta_f_lim[::-1]
-        
-        self.freq_lim = np.concatenate(((self.freq_mhz - self.band_mhz/2) - delta_f_lim_flipped,
-                                        (self.freq_mhz + self.band_mhz/2) + delta_f_lim))
 
-            
-    def get_frequency_limits(self, 
-                             sta_type : StationType,
-                             bandwidth : float) -> np.array:
+        delta_f_lim = self.get_frequency_limits(self.sta_type, self.band_mhz)
+        # delta_f_lim_flipped = np.flip(self.delta_f_lim,0)
+        delta_f_lim_flipped = delta_f_lim[::-1]
+
+        self.freq_lim = np.concatenate((
+            (self.freq_mhz - self.band_mhz / 2) - delta_f_lim_flipped,
+            (self.freq_mhz + self.band_mhz / 2) + delta_f_lim,
+        ))
+
+    def get_frequency_limits(
+        self,
+        sta_type: StationType,
+        bandwidth: float,
+    ) -> np.array:
         """
         Calculates the frequency limits of the spectrum emission masks. This
         implementation is valid only for bandwidths equal to 5, 10, 15 or 20 MHz.
         """
-        
+
         if sta_type is StationType.IMT_BS:
             # Mask delta f breaking limits [MHz]
             delta_f_lim = np.arange(0, 5.1, .1)
             delta_f_lim = np.append(delta_f_lim, 10)
-        else:        
+        else:
             delta_f_lim = np.array([0, 1, 5])
             if bandwidth == 5:
                 delta_f_lim = np.append(delta_f_lim, np.array([6, 10]))
@@ -79,28 +85,34 @@ class SpectralMask3Gpp(SpectralMask):
             else:
                 delta_f_lim = np.append(delta_f_lim, np.array([20, 25]))
         return delta_f_lim
-        
-            
-    def set_mask(self, power = 0):
-        emission_limits = self.get_emission_limits(self.sta_type,
-                                                   self.band_mhz,
-                                                   self.spurious_emissions)
-        self.p_tx = power - 10 * np.log10(self.band_mhz)
-        #emission_limits = np.flip(emission_limits, 0)
+
+    def set_mask(self, p_tx=0):
+        emission_limits = self.get_emission_limits(
+            self.sta_type,
+            self.band_mhz,
+            self.spurious_emissions,
+        )
+        self.p_tx = p_tx - 10 * np.log10(self.band_mhz)
+        # emission_limits = np.flip(emission_limits, 0)
         emission_limits_flipped = emission_limits[::-1]
-        self.mask_dbm = np.concatenate((emission_limits_flipped, 
-                                        np.array([self.p_tx]),
-                                        emission_limits))
+        self.mask_dbm = np.concatenate((
+            emission_limits_flipped,
+            np.array([self.p_tx]),
+            emission_limits,
+        ))
 
-
-    def get_emission_limits(self, 
-                            sta_type : StationType,
-                            bandwidth : float,
-                            spurious_emissions : float) -> np.array:
+    def get_emission_limits(
+        self,
+        sta_type: StationType,
+        bandwidth: float,
+        spurious_emissions: float,
+    ) -> np.array:
         if sta_type is StationType.IMT_BS:
             # emission limits in dBm/MHz
             emission_limits = 3 - 7 / 5 * (np.arange(.05, 5, .1) - .05)
-            emission_limits = np.append(emission_limits, np.array([-4, spurious_emissions]))
+            emission_limits = np.append(
+                emission_limits, np.array([-4, spurious_emissions]),
+            )
         else:
             if bandwidth == 5:
                 limit_r1 = np.array([-15])
@@ -110,6 +122,8 @@ class SpectralMask3Gpp(SpectralMask):
                 limit_r1 = np.array([-20])
             else:
                 limit_r1 = np.array([-21])
-            emission_limits = np.append(limit_r1 + 10*np.log10(1/0.03),
-                                        np.array([-10, -13, -25, spurious_emissions]))
+            emission_limits = np.append(
+                limit_r1 + 10 * np.log10(1 / 0.03),
+                np.array([-10, -13, -25, spurious_emissions]),
+            )
         return emission_limits
--- sharc/mask/spectral_mask_imt.py
+++ sharc/mask/spectral_mask_imt.py
@@ -11,17 +11,20 @@ from sharc.mask.spectral_mask import SpectralMask
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class SpectralMaskImt(SpectralMask):
     """
-    Implements spectral masks for IMT-2020 according to Document 5-1/36-E. 
-    The masks are in the document's tables 1 to 8.
-    
+    Implements spectral masks for IMT-2020 according to Document 5-1/36-E.
+        The masks are in the document's tables 1 to 8.
+    Implements alternative spectral masks for IMT-2020 for cases Document 5-1/36-E does not implement
+        according to Documents ITU-R SM.1541-6, ITU-R SM.1539-1 and ETSI TS 138 104 V16.6.0.
+        Uses alternative when: outdoor BS's with freq < 24.25GHz
+
     Attributes:
         spurious_emissions (float): level of power emissions at spurious
-            domain [dBm/MHz]. 
-        delta_f_lin (np.array): mask delta f breaking limits in MHz. Delta f 
-            values for which the spectral mask changes value. Hard coded as
-            [0, 20, 400]. In this context, delta f is the frequency distance to
+            domain [dBm/MHz].
+        delta_f_lin (np.array): mask delta f breaking limits in MHz. Delta f
+            values for which the spectral mask changes value. In this context, delta f is the frequency distance to
             the transmission's edge frequencies
         freq_lim (no.array): frequency values for which the spectral mask
             changes emission value
@@ -32,115 +35,267 @@ class SpectralMaskImt(SpectralMask):
         scenario (str): INDOOR or OUTDOOR scenario
         p_tx (float): station's transmit power in dBm/MHz
         mask_dbm (np.array): spectral mask emission values in dBm
+        alternative_mask_used (bool): represents whether the alternative mask should be used or not
+        ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE (int): A hardcoded value that specifies how many samples of a diagonal
+            line may be taken. Is needed because oob_power is calculated expecting rectangles, so we approximate
+            the diagonal with 'SAMPLESIZE' rectangles
     """
-    def __init__(self, 
-                 sta_type: StationType, 
-                 freq_mhz: float, 
-                 band_mhz: float, 
-                 spurious_emissions : float, 
-                 scenario = "OUTDOOR"):
+    ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE: int = 40
+
+    def __init__(
+        self,
+        sta_type: StationType,
+        freq_mhz: float,
+        band_mhz: float,
+        spurious_emissions: float,
+        scenario="OUTDOOR",
+    ):
         """
         Class constructor.
-        
+
         Parameters:
             sta_type (StationType): type of station to which consider the spectral
                 mask. Possible values are StationType.IMT_BS and StationType.
                 IMT_UE
             freq_mhz (float): center frequency of station in MHz
             band_mhs (float): transmitting bandwidth of station in MHz
-            spurious_emissions (float): level of spurious emissions [dBm/MHz]. 
+            spurious_emissions (float): level of spurious emissions [dBm/MHz].
             scenario (str): INDOOR or OUTDOOR scenario
         """
         # Spurious domain limits [dBm/MHz]
         self.spurious_emissions = spurious_emissions
+
+        # conditions to use alternative mask
+        self.alternative_mask_used = freq_mhz < 24250                 \
+            and scenario != "INDOOR"             \
+            and sta_type == StationType.IMT_BS   \
+            and spurious_emissions in [-13, -30]
+
         # Mask delta f breaking limits [MHz]
-        self.delta_f_lim = np.array([0, 20, 400])
-        
+        if self.alternative_mask_used:
+            self.delta_f_lim = self.get_alternative_mask_delta_f_lim(
+                freq_mhz, band_mhz,
+            )
+        else:
+            # use value from 5-1/36-E
+            self.delta_f_lim = np.array([0, 20, 400])
+
         # Attributes
         self.sta_type = sta_type
         self.scenario = scenario
         self.band_mhz = band_mhz
         self.freq_mhz = freq_mhz
-        
-        self.freq_lim = np.concatenate(((freq_mhz - band_mhz/2)-self.delta_f_lim[::-1],
-                                        (freq_mhz + band_mhz/2)+self.delta_f_lim))
-       
-        
-    def set_mask(self, power = 0):
+
+        self.freq_lim = np.concatenate((
+            (freq_mhz - band_mhz / 2) - self.delta_f_lim[::-1],
+            (freq_mhz + band_mhz / 2) + self.delta_f_lim,
+        ))
+
+    def set_mask(self, p_tx=0):
         """
-        Sets the spectral mask (mask_dbm attribute) based on station type, 
+        Sets the spectral mask (mask_dbm attribute) based on station type,
         operating frequency and transmit power.
-        
+
         Parameters:
-            power (float): station transmit power. Default = 0
+            p_tx (float): station transmit power. Default = 0
         """
-        self.p_tx = power - 10*np.log10(self.band_mhz)
-        
-        # Set new transmit power value       
+        if self.alternative_mask_used:
+            self.mask_dbm = self.get_alternative_mask_mask_dbm(p_tx)
+            return
+
+        self.p_tx = p_tx - 10 * np.log10(self.band_mhz)
+
+        # Set new transmit power value
         if self.sta_type is StationType.IMT_UE:
             # Table 8
             mask_dbm = np.array([-5, -13, self.spurious_emissions])
-            
-        elif self.sta_type is StationType.IMT_BS and self.scenario is "INDOOR":             
+
+        elif self.sta_type is StationType.IMT_BS and self.scenario == "INDOOR":
             # Table 1
             mask_dbm = np.array([-5, -13, self.spurious_emissions])
-            
+
         else:
-            
+
             if (self.freq_mhz > 24250 and self.freq_mhz < 33400):
-                if power >= 34.5:
+                if p_tx >= 34.5:
                     # Table 2
                     mask_dbm = np.array([-5, -13, self.spurious_emissions])
                 else:
                     # Table 3
-                    mask_dbm = np.array([-5, np.max((power-47.5,-20)), 
-                                          self.spurious_emissions])
+                    mask_dbm = np.array([
+                        -5, np.max((p_tx - 47.5, -20)),
+                        self.spurious_emissions,
+                    ])
             elif (self.freq_mhz > 37000 and self.freq_mhz < 52600):
-                if power >= 32.5:
+                if p_tx >= 32.5:
                     # Table 4
                     mask_dbm = np.array([-5, -13, self.spurious_emissions])
                 else:
                     # Table 5
-                    mask_dbm = np.array([-5, np.max((power-45.5,-20)), 
-                                          self.spurious_emissions])
+                    mask_dbm = np.array([
+                        -5, np.max((p_tx - 45.5, -20)),
+                        self.spurious_emissions,
+                    ])
             elif (self.freq_mhz > 66000 and self.freq_mhz < 86000):
-                if power >= 30.5:
+                if p_tx >= 30.5:
                     # Table 6
                     mask_dbm = np.array([-5, -13, self.spurious_emissions])
                 else:
                     # Table 7
-                    mask_dbm = np.array([-5, np.max((power-43.5,-20)), 
-                                          self.spurious_emissions])
+                    mask_dbm = np.array([
+                        -5, np.max((p_tx - 43.5, -20)),
+                        self.spurious_emissions,
+                    ])
             else:
-                # Dummy spectral mask, for testing purposes only
-                mask_dbm = np.array([-10, -20, -50])
-                 
-        self.mask_dbm = np.concatenate((mask_dbm[::-1],np.array([self.p_tx]),
-                                        mask_dbm))
-        
+                # this will only be reached when spurious emission has been manually set to something invalid and
+                # alternative mask should be used
+                raise ValueError(
+                    "SpectralMaskIMT cannot be used with current parameters. \
+                    You may have set spurious emission to a value not in [-13,-30]",
+                )
+
+        self.mask_dbm = np.concatenate((
+            mask_dbm[::-1], np.array([self.p_tx]),
+            mask_dbm,
+        ))
+
+    def get_alternative_mask_delta_f_lim(self, freq_mhz: float, band_mhz: float) -> np.array:
+        """
+            Implements spectral masks for IMT-2020 outdoor BS's when freq < 26GHz,
+                according to Documents ITU-R SM.1541-6, ITU-R SM.1539-1 and ETSI TS 138 104 V16.6.0.
+            Reference tables are:
+                - Table 1 in ITU-R SM.1541-6
+                - Table 2 in ITU-R SM.1539-1
+                - Table 6.6.4.2.1-2   in ETSI TS 138 104 V16.6.0
+                - Table 6.6.4.2.2.1-2 in ETSI TS 138 104 V16.6.0
+                - Table 6.6.5.2.1-1 in ETSI TS 138 104 V16.6.0 (to choose spurious emission, not an impementation table)
+                - Table 6.6.5.2.1-2 in ETSI TS 138 104 V16.6.0 (to choose spurious emission, not an impementation table)
+        """
+        # ITU-R SM.1539-1 Table 2
+        if (freq_mhz > 0.009 and freq_mhz <= 0.15):
+            B_L = 0.00025
+            B_U = 0.01
+        elif (freq_mhz > 0.15 and freq_mhz <= 30):
+            B_L = 0.004
+            B_U = 0.1
+        elif (freq_mhz > 30 and freq_mhz <= 1000):
+            B_L = 0.025
+            B_U = 10
+        elif (freq_mhz > 1000 and freq_mhz <= 3000):
+            B_L = 0.1
+            B_U = 50
+        elif (freq_mhz > 3000 and freq_mhz <= 10000):
+            B_L = 0.1
+            B_U = 100
+        elif (freq_mhz > 10000 and freq_mhz <= 15000):
+            B_L = 0.3
+            B_U = 250
+        elif (freq_mhz > 15000 and freq_mhz <= 26000):
+            B_L = 0.5
+            B_U = 500
+        else:
+            raise ValueError(f"Invalid frequency value {freq_mhz} for mask ITU-R SM.1539-1")
+
+        # ITU-R SM.1541-6 Table 1 (same as using only ITU-R SM.1539-1 Table 2, but with less hardcoded values)
+        B_L_separation = 2.5 * B_L
+        B_U_separation = 1.5 * band_mhz + B_U
+        B_N_separation = 2.5 * band_mhz
+
+        if band_mhz < B_L:
+            delta_f_spurious = B_L_separation
+        elif band_mhz > B_U:
+            delta_f_spurious = B_U_separation
+        else:
+            delta_f_spurious = B_N_separation
+
+        diagonal = np.array([
+            i * 5 / self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE for i in range(
+                self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE,
+            )
+        ])
+
+        # band/2 is subtracted from delta_f_spurious beacuse that specific interval is from frequency center
+        rest_of_oob_and_spurious = np.array(
+            [5, 10.0, delta_f_spurious - band_mhz / 2],
+        )
+
+        return np.concatenate((diagonal, rest_of_oob_and_spurious))
+
+    def get_alternative_mask_mask_dbm(self, power: float = 0) -> np.array:
+        """
+            Implements spectral masks for IMT-2020 outdoor BS's when freq < 26GHz,
+                according to Documents ITU-R SM.1541-6, ITU-R SM.1539-1 and ETSI TS 138 104 V16.6.0.
+            Reference tables are:
+                - Table 1 in ITU-R SM.1541-6
+                - Table 2 in ITU-R SM.1539-1
+                - Table 6.6.4.2.1-2   in ETSI TS 138 104 V16.6.0
+                - Table 6.6.4.2.2.1-2 in ETSI TS 138 104 V16.6.0
+                - Table 6.6.5.2.1-1   in ETSI TS 138 104 V16.6.0 (to choose spurious emission, not an impementation table)
+                - Table 6.6.5.2.1-2   in ETSI TS 138 104 V16.6.0 (to choose spurious emission, not an impementation table)
+        """
+        self.p_tx = power - 10 * np.log10(self.band_mhz)
+
+        if self.spurious_emissions == -13:
+            # use document ETSI TS 138 104 V16.6.0 Table 6.6.4.2.1-2
+            diagonal_samples = np.array([
+                -7 - 7 / 5 *
+                (i * 5 / self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE)
+                for i in range(self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE)
+            ])
+            rest_of_oob_and_spurious = np.array([
+                -14, -13, self.spurious_emissions,
+            ])
+            mask_dbm = np.concatenate(
+                (diagonal_samples, rest_of_oob_and_spurious),
+            )
+        elif self.spurious_emissions == -30:
+            # use document ETSI TS 138 104 V16.6.0 Table 6.6.4.2.2.1-2
+            diagonal_samples = np.array([
+                -7 - 7 / 5 *
+                (i * 5 / self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE)
+                for i in range(self.ALTERNATIVE_MASK_DIAGONAL_SAMPLESIZE)
+            ])
+            rest_of_oob_and_spurious = np.array(
+                [-14, -15, self.spurious_emissions],
+            )
+            mask_dbm = np.concatenate(
+                (diagonal_samples, rest_of_oob_and_spurious),
+            )
+        else:
+            raise ValueError(
+                "Alternative mask should only be used for spurious emissions -13 and -30",
+            )
+
+        return np.concatenate((
+            mask_dbm[::-1], np.array([self.p_tx]),
+            mask_dbm,
+        ))
+
+
 if __name__ == '__main__':
     # Initialize variables
     sta_type = StationType.IMT_BS
-    p_tx = 25.1
-    freq = 43000
-    band = 200
-    
+    p_tx = 34.061799739838875
+    freq = 9000
+    band = 100
+    spurious_emissions_dbm_mhz = -30
+
     # Create mask
-    msk = SpectralMaskImt(sta_type,freq,band)
+    msk = SpectralMaskImt(sta_type, freq, band, spurious_emissions_dbm_mhz)
     msk.set_mask(p_tx)
-    
+
     # Frequencies
-    freqs = np.linspace(-600,600,num=1000)+freq
-    
+    freqs = np.linspace(-600, 600, num=1000) + freq
+
     # Mask values
-    mask_val = np.ones_like(freqs)*msk.mask_dbm[0]
-    for k in range(len(msk.freq_lim)-1,-1,-1):
+    mask_val = np.ones_like(freqs) * msk.mask_dbm[0]
+    for k in range(len(msk.freq_lim) - 1, -1, -1):
         mask_val[np.where(freqs < msk.freq_lim[k])] = msk.mask_dbm[k]
-        
+
     # Plot
-    plt.plot(freqs,mask_val)
-    plt.xlim([freqs[0],freqs[-1]])
-    plt.xlabel("$\Delta$f [MHz]")
+    plt.plot(freqs, mask_val)
+    plt.xlim([freqs[0], freqs[-1]])
+    plt.xlabel(r"$\Delta$f [MHz]")
     plt.ylabel("Spectral Mask [dBc]")
     plt.grid()
     plt.show()
